9b6b238a092613a2dbdc327357ca9d1381979be7
sifeddineEddr <sifeddine.edr@gmail.com>
2023-05-02 23:17:37 +0100

    npm i ejs

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index', {text : 'test'})
+})
+
 app.listen(3000)
